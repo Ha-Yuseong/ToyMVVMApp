c0c08747aacdce2a9ba889bf95a435ba97c93c9a
sangjae <dlbia2009@gmail.com>
2023-12-01 09:02:54 +0900

    Jetpack Compose 공부

--- Study/JetpackCompose_practice/JetpackComposePractice/app/src/main/java/com/jetpackCompose/practice/MainActivity.kt
+++ Study/JetpackCompose_practice/JetpackComposePractice/app/src/main/java/com/jetpackCompose/practice/MainActivity.kt
@@ -3,14 +3,26 @@ package com.jetpackCompose.practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jetpackCompose.practice.PreviewParameter.Test
+import com.jetpackCompose.practice.PreviewParameter.TestPreviewProvider
 import com.jetpackCompose.practice.ui.theme.JetpackPracticeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +34,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Message()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Preview
+    @Composable
+    fun Message(){
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+            .padding(16.dp)
+            , horizontalAlignment = Alignment.CenterHorizontally) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackPracticeTheme {
-        Greeting("Android")
+            Text("Hello")
+            Text("World22")
+        }
     }
+
+    @Preview
+    @Composable
+    fun previewParamTest(@PreviewParameter(TestPreviewProvider::class, 4) test : Test){
+        showMessage(test = test)
+    }
+
+    @Composable
+    fun showMessage(test : Test){
+        Column {
+            Text(text = test.testInput, fontSize = 100.sp)
+        }
+    }
+
 }
